1606eb0bf8092ada82c91db2580bd80082ec51db
williamlamjy <e0543903@u.nus.edu>
2021-08-25 19:55:41 +0800

    Added mark as done functionalities Added task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,23 +11,34 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Luke");
         System.out.println("What can I do for you?");
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         Scanner in = new Scanner(System.in);
         String line;
         line = in.nextLine();
-        int noOfBooks = 0;
-        while (!Objects.equals(line, "bye")) {
-            if (Objects.equals(line, "list")) {
-                for (int i = 0; i < noOfBooks; i++) {
-                    System.out.println((i + 1) + ": " + list[i]);
+        int noOfTasks = 0;
+        while (!line.equals("bye")) {
+            if (line.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < noOfTasks; i++) {
+                    System.out.println((i + 1) + ". " + "[" + list[i].getStatusIcon() + "]" + list[i].description);
                 }
+                noOfTasks -= 1;
             }
-            list[noOfBooks] = line;
-            if (!Objects.equals(line, "list")) {
+            if (!line.equals("list") && !(line.contains("done"))) {
+                list[noOfTasks] = new Task(line);
                 System.out.println("added: " + line);
             }
+            if (line.contains("done")) {
+                int a = line.indexOf(" ");
+                String taskNo = line.substring(a + 1);
+                int taskNoComplete = Integer.parseInt(taskNo) - 1;
+                list[taskNoComplete].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(" [X] " + list[taskNoComplete].description);
+                noOfTasks -= 1;
+            }
             line = in.nextLine();
-            noOfBooks++;
+            noOfTasks++;
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
