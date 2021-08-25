ee2197748088b6d536948059d24b6f7e4222bef2
williamlamjy <e0543903@u.nus.edu>
2021-08-25 16:43:52 +0800

    Greet,Echo,Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,6 +10,13 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        Scanner in = new Scanner(System.in);
+        String line;
+        line= in.nextLine();
+        while(!Objects.equals(line, "bye")){
+            System.out.println(line);
+            line=in.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
